7d8c67940ec7af2cae7314acca5e2140af9718de
Martin Lopatář <lopin@dataplex.cz>
2018-02-15 17:36:43 +0100

    build rewritten

--- kernel-builder/scripts/build.sh
+++ kernel-builder/scripts/build.sh
@@ -2,36 +2,51 @@
 set -x
 set -e
 
-# if MAKEFLAGS not set, USE '-j<NUMBER_OF_PROCESSORS*2>'
+date '+%F %T' #date
+
+# if MAKEFLAGS are not set, USE '-j<NUMBER_OF_PROCESSORS*2>'
 : "${MAKEFLAGS:=-j$(($(nproc)*2))}"
+export MAKEFLAGS
 
 # extract the kernel source code
 cd /build/linux
-dpkg-source -x ./*.dsc
+dpkg-source -x /SRC/linux/*.dsc
 
-# go to linux kernel source directory (name depends on version)
+# go to linux kernel source directory (name depends on what the current version is)
 cd -P /build/linux/linux-*
+linux_ver_dir="$PWD"
+
 
 # add custom patches (if any) and generate corresponding changelog entry
-if [ -e /build/patches/* ]; then
+if [ -e /SRC/patches/* ]; then
 	echo -e "\n# Custom patches:" >>./debian/patches/series
-
-	quilt import /build/patches/*
-
+	quilt import /SRC/patches/*
 	# create changelog entry from patches descriptions
 	dep3changelog "$(quilt unapplied)" -- $DEBCHANGE_OPTIONS
-
 	# apply custom patches
 	quilt push -a
 else
 	debchange $DEBCHANGE_OPTIONS 'Build with unchanged sources'
 fi
 
-# build
+
+# BUILD
+
+date '+%F %T' #date
+# apply environment variables configuration (e.g. DEBIAN_KERNEL_DISABLE_DEBUG)
+make -f debian/rules source ||:
+date '+%F %T' #date
 make -f debian/rules.gen setup_${ARCH}_${FEATURESET}_${FLAVOUR}
+date '+%F %T' #date
+# make packages
 make -f debian/rules.gen binary-arch_${ARCH}_${FEATURESET}_${FLAVOUR} binary-indep_${FEATURESET}
+date '+%F %T' #date
+
 
 # move output deb packeges to /build/out directory
-mkdir /build/out
-mv /build/linux/*.deb  /build/out/
+mkdir /build/pkgs /build/linux/.workdir ||:
+cd /build/linux
+mv "$linux_ver_dir" /build/linux/.workdir/
+mv /build/linux/* /build/pkgs/
+date '+%F %T' #date
 
